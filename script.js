571af8f1d9169235d46de1b5e79ff003390677e5
RiSenC <162602946+RiSenC@users.noreply.github.com>
2026-02-11 15:30:20 +0300

    js update and some fixes

--- script.js
+++ script.js
@@ -1,65 +1,166 @@
-let abButton = document.getElementById("statsABbutton");
-let rbButton = document.getElementById("statsRBbutton");
-let topButton = document.getElementById("stats_TOP_button");
-let baseButton = document.getElementById("stats_base_button");
+let stats_abBtn = document.getElementById("statsABbutton");
+let stats_rbBtn = document.getElementById("statsRBbutton");
+let stats_topBtn = document.getElementById("stats_top_button");
+let stats_baseBtn = document.getElementById("stats_base_button");
 
-let stat_kmh = document.getElementById("stat_kmh");
-let stat_engine = document.getElementById("stat_engine");
-let stat_total = document.getElementById("stat_total");
+let stats_kmh = document.getElementById("stats_kmh");
+let stats_engine = document.getElementById("stats_engine");
+let stats_total = document.getElementById("stats_total");
 
-let stat_kmh_values = [35, 38]; // РБ, АБ
-let stat_engine_values = [500, 442, 795, 646]; // РБ-эталон, РБ-базовый, АБ-эталон, АБ-базовый
-let stat_total_values = [13.2, 11.7, 21, 17.1];// РБ-эталон, РБ-базовый, АБ-эталон, АБ-базовый
+let stats_kmh_values = [35, 38]; // РБ, АБ
+let stats_engine_values = [500, 442, 795, 646]; // РБ-эталон, РБ-базовый, АБ-эталон, АБ-базовый
+let stats_total_values = [13.2, 11.7, 21, 17.1];// РБ-эталон, РБ-базовый, АБ-эталон, АБ-базовый
 
-let mode_flag = 0;      // 0 - РБ; 1 - АБ
-let condition_flag = 0; // 0 - Эталон; 1 - базов
+let stats_modeFlag = 0;      // 0 - РБ; 1 - АБ
+let stats_conditionFlag = 0; // 0 - Эталон; 1 - базовый
 
-rbButton.onclick = rbStatsButton;
-abButton.onclick = abStatsButton;
-
-function rbStatsButton() {
-    if(mode_flag)
+function changeStatValues() {
+    if(!stats_conditionFlag)
     {
-        mode_flag = 0;
-        abButton.classList.remove("button-active");
-        rbButton.classList.add("button-active")
-        stat_kmh.textContent = stat_kmh_values[0]+" км/ч";
-        if(!condition_flag)
+        if(!stats_modeFlag)
         {
-            stat_engine.textContent = stat_engine_values[0]+" л.с.";
-            stat_total.textContent = stat_total_values[0]+" л.с./т";
+            stats_engine.textContent = stats_engine_values[0]+" л.с.";
+            stats_total.textContent = stats_total_values[0]+" л.с./т";
         }
         else
         {
-            stat_engine.textContent = stat_engine_values[1]+" л.с.";
-            stat_total.textContent = stat_total_value[1]+" л.с./т";
+            stats_engine.textContent = stats_engine_values[2]+" л.с.";
+            stats_total.textContent = stats_total_values[2]+" л.с./т";
         }
-        stat_kmh.style.color = "red";
-        stat_engine.style.color = "red";
-        stat_total.style.color = "red";
+    }
+    else
+    {
+        if(!stats_modeFlag)
+        {
+            stats_engine.textContent = stats_engine_values[1]+" л.с.";
+            stats_total.textContent = stats_total_values[1]+" л.с./т";
+        }
+        else
+        {
+            stats_engine.textContent = stats_engine_values[3]+" л.с.";
+            stats_total.textContent = stats_total_values[3]+" л.с./т";
+        }
+    }
+}
+
+function rbStatsButton() {
+    if(stats_modeFlag)
+    {
+        stats_modeFlag = 0;
+        stats_abBtn.classList.remove("button-active");
+        stats_rbBtn.classList.add("button-active");
+        stats_kmh.textContent = stats_kmh_values[0]+" км/ч";
+        changeStatValues();
+        stats_kmh.style.color = "red";
+        stats_engine.style.color = "red";
+        stats_total.style.color = "red";
     }
 }
 
 function abStatsButton() {
-    if(!mode_flag)
-        {
-            mode_flag = 1;
-            abButton.classList.add("button-active");
-            rbButton.classList.remove("button-active")
-            stat_kmh.textContent = stat_kmh_values[1]+" км/ч";
-            if(!condition_flag)
-            {
-                stat_engine.textContent = stat_engine_values[3]+" л.с.";
-                stat_total.textContent = stat_total_values[3]+" л.с./т";
-            }
-            else
-            {
-                stat_engine.textContent = stat_engine_values[4]+" л.с.";
-                stat_total.textContent = stat_total_value[4]+" л.с./т";
-            }
-            stat_kmh.style.color = "green";
-            stat_engine.style.color = "green";
-            stat_total.style.color = "green";
-        }
+    if(!stats_modeFlag)
+    {
+        stats_modeFlag = 1;
+        stats_abBtn.classList.add("button-active");
+        stats_rbBtn.classList.remove("button-active");
+        stats_kmh.textContent = stats_kmh_values[1]+" км/ч";
+        changeStatValues();
+        stats_kmh.style.color = "green";
+        stats_engine.style.color = "green";
+        stats_total.style.color = "green";
+    }
 }
 
+function topStatsButton() {
+    if(stats_conditionFlag)
+    {
+        stats_conditionFlag = 0;
+        stats_baseBtn.classList.remove("button-active");
+        stats_topBtn.classList.add("button-active");
+        changeStatValues();
+        stats_engine.style.color = "green";
+        stats_total.style.color = "green";
+    }
+}
+
+function baseStatsButton() {
+    if(!stats_conditionFlag)
+    {
+        stats_conditionFlag = 1;
+        stats_baseBtn.classList.add("button-active");
+        stats_topBtn.classList.remove("button-active");
+        changeStatValues();
+        stats_engine.style.color = "red";
+        stats_total.style.color = "red";
+    }
+}
+
+//let wpnry_aceBtn = document.getElementById("wpnry_aceButton");
+//let wpnry_lohiBtn = document.getElementById("wpnry_lohiButton");
+let wpnry_abBtn = document.getElementById("wpnry_abButton");
+let wpnry_rbBtn = document.getElementById("wpnry_rbButton");
+let wpnry_topBtn = document.getElementById("wpnry_topButton");
+let wpnry_baseBtn = document.getElementById("wpnry_baseButton");
+
+let wpnry_reload = document.getElementById("wpnry_reload");
+let wpnry_horizontal = document.getElementById("wpnry_horizontal");
+let wpnry_vertical = document.getElementById("wpnry_vertical");
+
+let wpnry_reload_values;
+let wpnry_horizontal_values;
+let wpnry_vertical_values;
+
+let wpnry_crewFlag = 0;      // 0 - Асы; 1 - лохи
+let wpnry_modeFlag = 0;      // 0 - РБ; 1 - АБ
+let wpnry_conditionFlag = 0; // 0 - Эталон; 1 - базовый
+
+function rbWpnryButton() {
+    if(wpnry_modeFlag)
+    {
+        wpnry_abBtn.classList.remove("button-active");
+        wpnry_modeFlag = 0;
+        wpnry_rbBtn.classList.add("button-active");
+        // wpnry_kmh.textContent = stats_kmh_values[0]+" км/ч";
+        // changeStatValues();
+        // wpnry_kmh.style.color = "red";
+        // wpnry_engine.style.color = "red";
+        // wpnry_total.style.color = "red";
+    }
+}
+
+function abWpnryButton() {
+    if(!wpnry_modeFlag)
+    {
+        wpnry_modeFlag = 1;
+        wpnry_abBtn.classList.add("button-active");
+        wpnry_rbBtn.classList.remove("button-active");
+        //wpnry_kmh.textContent = stats_kmh_values[1]+" км/ч";
+        //changeStatValues();
+        //wpnry_kmh.style.color = "green";
+        //wpnry_engine.style.color = "green";
+    }
+}
+
+function topWpnryButton() {
+    if(wpnry_conditionFlag)
+    {
+        wpnry_conditionFlag = 0;
+        wpnry_baseBtn.classList.remove("button-active");
+        wpnry_topBtn.classList.add("button-active");
+        // changeStatValues();
+        // wpnry_engine.style.color = "green";
+        // wpnry_total.style.color = "green";
+    }
+}
+
+function baseWpnryButton() {
+    if(!wpnry_conditionFlag)
+    {
+        wpnry_conditionFlag = 1;
+        wpnry_baseBtn.classList.add("button-active");
+        wpnry_topBtn.classList.remove("button-active");
+        //changeStatValues();
+        //wpnry_engine.style.color = "red";
+        //wpnry_total.style.color = "red";
+    }
+}
